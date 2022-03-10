1099227f396c4017ac3c73c4896032074c29c90d
Shoyayeb <sa.fine@outlook.com>
2022-03-10 16:05:39 +0600

    express and cors added and root server working

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log('====================================');
-console.log("working");
-console.log('====================================');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("server running")
+});
+app.listen(port, () => {
+    console.log('====================================');
+    console.log("server running on ", port);
+    console.log('====================================');
+})
