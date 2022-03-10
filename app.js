5a1416335e9765a4de5d46a0c41122b78be6dbb6
Shoyayeb <sa.fine@outlook.com>
2022-03-10 16:21:45 +0600

    mongodb added

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+
+require("dotenv").config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 const port = 5000;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4f4qc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('====================================');
+    console.log(collection);
+    console.log('====================================');
+    // perform actions on the collection object
+    client.close();
+});
 
 app.get("/", (req, res) => {
     res.send("server running")
