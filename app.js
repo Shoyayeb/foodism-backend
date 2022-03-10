c0c32f5af3148207f93f50215a5518e11615296e
Shoyayeb <sa.fine@outlook.com>
2022-03-10 16:32:25 +0600

    mongodb connect and get api for foods

--- app.js
+++ app.js
@@ -10,18 +10,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4f4qc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('====================================');
-    console.log(collection);
-    console.log('====================================');
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        // connecting to mongodb
+        await client.connect();
+        const database = client.db("foodism");
+        const foodsCollection = database.collection("foods");
+
+        // get api to getting foods
+        app.get("/foods", async (req, res) => {
+            const cursor = foodsCollection.find({});
+            const foods = await cursor.toArray();
+            res.send(foods);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
     res.send("server running")
